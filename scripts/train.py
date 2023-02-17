7cec8bc4d575b7356117af2dece7c39de551bdac
jiahe.xu <jiahe.xu@faradayfuture.com>
2023-02-17 12:57:08 -0500

    setup

---
+++ scripts/train.py
@@ -0,0 +1,99 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import numpy as np
+import pickle
+import sys
+import argparse
+import traceback
+
+
+
+
+def main():
+
+    #####################
+    # getting args
+    #####################
+
+    parser = argparse.ArgumentParser(
+        # Show default value in the help doc.
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,)
+
+    parser.add_argument(
+        '-c',
+        '--config',
+        nargs='*',
+        help=('Path to the job data config file. (relative to working directory) '))
+
+    parser.add_argument(
+        '-o',
+        '--output_dir',
+        default='output',
+        help=
+        ('Directory to output trained policies, logs, and plots. A subdirectory is created for each job.(relative to working directory) '))
+
+    parser.add_argument('--use_gpu', action="store_true")
+    parser.add_argument('-frac', '--gpu_frac', type=float, default=0.9)
+    general_args = parser.parse_args()
+
+    #####################
+    # job configs
+    #####################
+
+    # Get the job config files
+    jobs = config_reader.process_config_files(general_args.config)
+    assert jobs, 'No jobs found from config.'
+
+    # Create the output directory if not present.
+    output_dir = general_args.output_dir
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    output_dir = os.path.abspath(output_dir)
+
+    # Run separate experiment for each variant in the config
+    for index, job in enumerate(jobs):
+
+        #add an index to jobname, if there is more than 1 job
+        if len(jobs)>1:
+            job['job_name'] = '{}_{}'.format(job['job_name'], index)
+
+        #convert job dictionary to different format
+        args_list = config_dict_to_flags(job)
+        args = convert_to_parser_args(args_list)
+
+        #copy some general_args into args
+        args.use_gpu = general_args.use_gpu
+        args.gpu_frac = general_args.gpu_frac
+
+        #directory name for this experiment
+        job['output_dir'] = os.path.join(output_dir, job['job_name'])
+
+        ################
+        ### run job
+        ################
+
+        try:
+            run_job(args, job['output_dir'])
+        except (KeyboardInterrupt, SystemExit):
+            print('Terminating...')
+            sys.exit(0)
+        except Exception as e:
+            print('ERROR: Exception occured while running a job....')
+            traceback.print_exc()
+
+
+if __name__ == '__main__':
+    main()
